bda27e288486d0cd5e2138683ea10a04f87e6678
Sagar Wadhwa <74873531+Sagar-Wadhwa-726@users.noreply.github.com>
2021-07-24 00:37:44 +0530

    Delete TASK-2/my-app. directory

--- TASK-2/my-app./src/parts/ppl.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const ppl = (props) => {
-    return(
-        <div style={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
-            {props.children}
-        </div>
-    )
-};
-export default ppl;
